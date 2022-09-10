a492f26013584db0879948f30851cb5b3e969828
Oluwafimidara M. Ajetomobi <111705720+Darayb@users.noreply.github.com>
2022-09-10 12:17:54 +0100

    Add files via upload

---
+++ New folder/ui.js
@@ -0,0 +1,11 @@
+
+function start1(){
+    document.getElementById('form').style.visibility='visible'
+    document.getElementById('whole').style.opacity=0.3;
+
+}
+function back1(){
+    document.getElementById('form').style.visibility='hidden'
+    document.getElementById('whole').style.opacity=1;
+
+}
